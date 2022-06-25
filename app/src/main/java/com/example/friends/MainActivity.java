8e14515a8176e7a11b0f52a779fc955d65bdfa83
Barjatya <nrbbarjatya@gmail.com>
2022-06-26 04:35:58 +0530

    first commit

---
+++ app/src/main/java/com/example/friends/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.friends;
+
+import android.os.Bundle;
+
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+    }
+}
+
