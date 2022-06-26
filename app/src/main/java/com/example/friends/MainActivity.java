c219a35b6584234543a0986bdcc45953d684539c
Barjatya <nrbbarjatya@gmail.com>
2022-06-26 22:35:08 +0530

    UI updated

--- app/src/main/java/com/example/friends/MainActivity.java
+++ app/src/main/java/com/example/friends/MainActivity.java
@@ -1,6 +1,12 @@
 package com.example.friends;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
 
 import com.google.android.material.snackbar.Snackbar;
 
@@ -9,11 +15,33 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button sendOtpButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Window window = this.getWindow();
+
+// clear FLAG_TRANSLUCENT_STATUS flag:
+        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+
+// add FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS flag to the window
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+
+// finally change the color
+        window.setStatusBarColor(Color.TRANSPARENT);
+
+        sendOtpButton=findViewById(R.id.send_otp_button);
+
+        sendOtpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent =new Intent(MainActivity.this,OtpAuthActivity.class);
+                startActivity(intent);
+            }
+        });
 
 
 
